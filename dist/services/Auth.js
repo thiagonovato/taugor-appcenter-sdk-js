b834b2549e28e9b5d20e8d1d0937ae76ed59c82c
Guilherme Ferreira <guilherme.ferreira@zerobyte.app>
2019-12-17 00:29:21 -0300

    fixed dist

--- dist/services/Auth.js
+++ dist/services/Auth.js
@@ -5,7 +5,7 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports["default"] = void 0;
 
-var _functions = require("./functions.js");
+var _functions = require("../functions.js");
 
 var Auth = function Auth() {
   var _private = {
