75690f5afb358edbc3db5c1959929f6eb72218d3
brunoteixeirasilva <bruno.teixeira.silva@gmail.com>
2020-01-30 22:56:25 -0300

    sa

--- node_modules/@zerobytes/firebase-basic-service/index.js
+++ node_modules/@zerobytes/firebase-basic-service/index.js
@@ -2,9 +2,11 @@ const MOD = require('./src/BasicService');
 const BasicService = MOD.BasicService;
 const uid = MOD.uid;
 const normalizeProps = MOD.normalizeProps;
+const createIndex = MOD.createIndex;
 
 module.exports = {
 	BasicService,
 	normalizeProps,
-	uid
+	uid,
+	createIndex
 };
