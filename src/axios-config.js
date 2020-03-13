7763019557c2662a54f3c4f6155f3b657ed95713
Guilherme Ferreira <guilherme.ferreira@zerobyte.app>
2020-03-13 15:48:46 -0300

    axios config

--- src/axios-config.js
+++ src/axios-config.js
@@ -1,7 +1,13 @@
 import axios from "axios";
 
 const api = axios.create({
-	baseURL: "https://us-central1-taugor-appcenter.cloudfunctions.net/webApi/v1"
+	baseURL:
+		"https://us-central1-taugor-appcenter.cloudfunctions.net/webApi/v1",
+	headers: {
+		common: {
+			"Access-Control-Allow-Origin": "*"
+		}
+	}
 });
 
 export default api;
